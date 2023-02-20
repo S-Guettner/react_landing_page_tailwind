4f3b0cdade43c86fe4f962fc1bf5c8b5f95bab44
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-20 15:13:41 +0100

    main

--- src/assets/components/Main/Main.jsx
+++ src/assets/components/Main/Main.jsx
@@ -3,8 +3,8 @@ import MainText from './MainText.jsx'
 const Main = () => {
     return ( 
         <main>
-            <p className="text-center">What We Do</p>
-            <h2 className="text-center mb-4">We've got everything you need to launch and grow your business</h2>
+            <p className="text-center text-green-700 font-bold">What We Do</p>
+            <h2 className="text-center mb-8 text-xl font-bold">We've got everything you need to launch and grow your business</h2>
             <section className="grid grid-cols-2 text-center">
                 <MainText title="Brand identity" iconSrc="/images/icons8-etikett-24.png"/>
                 <MainText title="Illustration" iconSrc="/images/icons8-design-24.png"/>
--- src/assets/components/Main/MainText.jsx
+++ src/assets/components/Main/MainText.jsx
@@ -2,7 +2,7 @@ const MainText = (props) => {
     return ( 
         <div className="mb-4">
             <img className="inline" src={props.iconSrc} alt="" />
-            <p className="inline">{props.title}</p>
+            <p className="inline font-bold">{props.title}</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vitae!</p>
         </div>
      );
