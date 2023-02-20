4bb2291973ef9ba556350013e5a61bc9ffc51849
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-20 14:23:42 +0100

    header

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,10 +1,17 @@
+import HeaderNumbers from "./HeaderNumbers"
+
 const Header = () => {
     return ( 
         <header className="text-center bg-green-700">
             <p className="pt-5 mb-2 font-bold">Hello There</p>
             <h1 className="text-5xl text-white mb-4 border-b mx-48 pb-6">We Are Glint</h1>
-            <p className="w-3/4 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut, nam maxime aperiam ab inventore explicabo id dolor beatae laudantium enim, minima commodi, libero autem odio sunt natus culpa reprehenderit!</p>
-        
+            <p className="w-3/4 mx-auto mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut, nam maxime aperiam ab inventore explicabo id dolor beatae laudantium enim, minima commodi, libero autem odio sunt natus culpa reprehenderit!</p>
+                <section className="flex justify-between pb-4">
+                    <HeaderNumbers number="127" />
+                    <HeaderNumbers number="1505" />
+                    <HeaderNumbers number="109" />
+                    <HeaderNumbers number="102" />
+                </section>
         </header>
      );
 }
---
+++ src/assets/components/Header/HeaderNumbers.jsx
@@ -0,0 +1,10 @@
+const HeaderNumbers = (props) => {
+    return ( 
+        <div className="border-l">
+            <p className="text-white text-xl font-bold">{props.number}</p>
+            <p className="text-center pl-2">Lorem, ipsum dolor.</p>
+        </div>
+     );
+}
+ 
+export default HeaderNumbers;
