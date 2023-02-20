6260e4ca28982ac75e1e6af81616fe1587575d81
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-20 14:14:16 +0100

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './assets/components/Header/Header.jsx'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return ( 
+        <header className="text-center bg-green-700">
+            <p className="pt-5 mb-2 font-bold">Hello There</p>
+            <h1 className="text-5xl text-white mb-4 border-b mx-48 pb-6">We Are Glint</h1>
+            <p className="w-3/4 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut, nam maxime aperiam ab inventore explicabo id dolor beatae laudantium enim, minima commodi, libero autem odio sunt natus culpa reprehenderit!</p>
+        
+        </header>
+     );
+}
+ 
+export default Header;
